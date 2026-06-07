15368594cbb8888fa51edb5479a7aa7b03000bb3
indahnadia219-sketch <indahnadia219@gmail.com>
2026-06-07 20:49:17 +0700

    kandang

---
+++ KANDANG.py
@@ -0,0 +1,15 @@
+from BINATANG import Burung, Ikan
+
+class Kandang:
+    
+    def _init_(self, nama_kandang):
+        self.nama_kandang = nama_kandang
+        self.hewan_list = []
+    
+    def tambah_hewan(self, hewan):
+        self.hewan_list.append(hewan)
+    
+class BersihkanKandang:
+
+    def bersihkan(self, kandang: Kandang):
+        print(f"{kandang.nama_kandang} Telah dibersihkan.")
