b10cf27aaeacfd20c02fc4c70c98ee424ad8b3e7
udzmanufuus0702-bit <udzmanufuus0702@gmail.com>
2026-06-08 23:37:49 +0700

    Update BINATANG.py

--- BINATANG.py
+++ BINATANG.py
@@ -1,13 +1,21 @@
 from HEWAN import Hewan, HewanTerbang
 
 class Burung(HewanTerbang):
-    
+    def __init__(self, nama, jenis):
+        super().__init__()
+        self.nama = nama
+        self.jenis = jenis
+
     def makan(self):
         print(f"{self.nama} sedang makan biji-bijian.")
-        
+
     def terbang(self):
         print(f"{self.nama} sedang terbang di udara.")
 
 class Ikan(Hewan):
+    def __init__(self, nama, jenis):
+        self.nama = nama
+        self.jenis = jenis
+
     def makan(self):
         print(f"{self.nama} sedang makan pelet ikan.")
